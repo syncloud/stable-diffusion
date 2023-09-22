86bf45b788fc61d66e4cde3ef21395d5a00c56ba
Boris Rybalkin <ribalkin@gmail.com>
2023-09-22 17:49:47 +0100

    unix socket support

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -2,7 +2,9 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
+export GRADIO_SERVER_NAME=/var/snap/stable-diffusion/common/web.socket
+/bin/rm -f $GRADIO_SERVER_NAME
 exec ${DIR}/python/bin/python -u ${DIR}/python/webui/launch.py \
   --skip-prepare-environment \
-  --ldap-uri="ldap://localhost:389" \
-  --ldap-bind-dn="cn={username},ou=users,dc=syncloud,dc=org"
+  --ldap-uri 'ldap://localhost:389' \
+  --ldap-bind-dn 'cn={username},ou=users,dc=syncloud,dc=org'
--- python/build-inside.sh
+++ python/build-inside.sh
@@ -7,6 +7,8 @@ mv stable-diffusion-webui-* webui
 cd webui
 export COMMANDLINE_ARGS="--skip-torch-cuda-test"
 python -c 'from modules import launch_utils; launch_utils.prepare_environment()'
+cd /usr/local/lib/python3.8/site-packages
+git apply /patch/gradio.patch
 #pip install -r /requirements.txt
 python -m pip cache purge
 rm -rf /var/lib/apt/lists/*
