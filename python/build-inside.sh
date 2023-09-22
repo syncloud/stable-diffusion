9642a576b5c771d36ca23465c935a6b4c6cda65a
Boris Rybalkin <ribalkin@gmail.com>
2023-09-22 18:48:19 +0100

    fix patch

--- python/build-inside.sh
+++ python/build-inside.sh
@@ -8,7 +8,7 @@ cd webui
 export COMMANDLINE_ARGS="--skip-torch-cuda-test"
 python -c 'from modules import launch_utils; launch_utils.prepare_environment()'
 cd /usr/local/lib/python3.8/site-packages
-git apply /patch/gradio.patch
+git apply --ignore-space-change --ignore-whitespace /patch/gradio.patch
 #pip install -r /requirements.txt
 python -m pip cache purge
 rm -rf /var/lib/apt/lists/*
