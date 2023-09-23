9788d595ff3102db07b4acc28cab46a5a5958d0a
Boris Rybalkin <ribalkin@gmail.com>
2023-09-23 21:25:11 +0100

    xformers

--- python/build-inside.sh
+++ python/build-inside.sh
@@ -1,14 +1,25 @@
 #!/bin/sh -ex
+
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
 apt update -y
 apt install -y wget git
 wget https://github.com/cyberb/stable-diffusion-webui/archive/refs/heads/master.tar.gz --progress=dot:giga
 tar xf master.tar.gz
 mv stable-diffusion-webui-* webui
-cd webui
+cd $DIR/webui
 export COMMANDLINE_ARGS="--skip-torch-cuda-test"
 python -c 'from modules import launch_utils; launch_utils.prepare_environment()'
 cd /usr/local/lib/python3.8/site-packages
 git apply --ignore-space-change --ignore-whitespace /patch/gradio.patch
-#pip install -r /requirements.txt
+
+cd $DIR/webui
+cd repositories
+git clone https://github.com/facebookresearch/xformers.git
+cd xformers
+git submodule update --init --recursive
+pip install -r requirements.txt
+pip install -e .
+
 python -m pip cache purge
 rm -rf /var/lib/apt/lists/*
