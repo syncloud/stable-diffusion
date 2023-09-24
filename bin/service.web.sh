34002e94fb47a40282011b9b5e341886c0750194
Boris Rybalkin <ribalkin@gmail.com>
2023-09-24 14:09:15 +0100

    git

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -3,6 +3,7 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 export GRADIO_SERVER_NAME=/var/snap/stable-diffusion/common/web.socket
+export PATH=$PATH:${DIR}/python/bin
 /bin/rm -f $GRADIO_SERVER_NAME
 exec ${DIR}/python/bin/python -u ${DIR}/python/webui/launch.py \
   --skip-prepare-environment \
--- python/build.sh
+++ python/build.sh
@@ -16,5 +16,6 @@ docker export python -o python.tar
 tar xf python.tar
 rm -rf python.tar
 cp ${DIR}/python ${BUILD_DIR}/bin/
+cp ${DIR}/git ${BUILD_DIR}/bin/
 ls -la ${BUILD_DIR}/bin
 rm -rf ${BUILD_DIR}/usr/src
