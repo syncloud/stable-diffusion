7fc1827c6aced8bf1fbce9580cc9b74e9d7124e0
Boris Rybalkin <ribalkin@gmail.com>
2023-09-25 07:00:29 +0100

    codeformer

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -8,5 +8,6 @@ export PATH=$PATH:${DIR}/python/bin
 exec ${DIR}/python/bin/python -u ${DIR}/python/webui/launch.py \
   --skip-prepare-environment \
   --hypernetwork-dir '/var/snap/stable-diffusion/current/hypernetwork' \
+  --codeformer-models-path '/var/snap/stable-diffusion/current/models/codeformer' \
   --ldap-uri 'ldap://localhost:389' \
   --ldap-bind-dn 'cn={username},ou=users,dc=syncloud,dc=org'
