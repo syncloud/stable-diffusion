b188c691e091d6b880563096c0f0a7163b1893eb
Boris Rybalkin <ribalkin@gmail.com>
2023-09-29 17:11:11 +0100

    fix dir

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -8,6 +8,6 @@ export PATH=$PATH:${DIR}/python/bin
 exec ${DIR}/python/bin/python -u ${DIR}/python/webui/launch.py \
   --skip-prepare-environment \
   --data-dir '/var/snap/stable-diffusion/current' \
-  --ckpt '$DIR/python/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors'
+  --ckpt "$DIR/python/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors"
   --ldap-uri 'ldap://localhost:389' \
   --ldap-bind-dn 'cn={username},ou=users,dc=syncloud,dc=org'
