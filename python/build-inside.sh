d4b0c6e3c41a24917f7e12b2b6daee7159cabb83
Boris Rybalkin <ribalkin@gmail.com>
2023-10-08 16:03:05 +0100

    fix unix socket start

--- python/build-inside.sh
+++ python/build-inside.sh
@@ -4,6 +4,8 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 
 apt update -y
 apt install -y wget git build-essential libgl1 libglib2.0-0
+pip install -r /requirements.txt
+
 wget https://github.com/cyberb/stable-diffusion-webui/archive/refs/heads/master.tar.gz --progress=dot:giga
 tar xf master.tar.gz
 mv stable-diffusion-webui-* webui
@@ -12,7 +14,6 @@ export COMMANDLINE_ARGS="--skip-torch-cuda-test"
 python -c 'from modules import launch_utils; launch_utils.prepare_environment()'
 cd /usr/local/lib/python3*/site-packages
 git apply --ignore-space-change --ignore-whitespace /patch/gradio.patch
-
 
 mkdir -p $DIR/models/Stable-diffusion
 wget https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.safetensors -O $DIR/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors
