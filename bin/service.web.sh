2cf6aa7e33a946a45111b936482701f7977f8333
Boris Rybalkin <ribalkin@gmail.com>
2023-10-09 21:46:51 +0100

    fix current dir

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -2,10 +2,13 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
+export COMMANDLINE_ARGS=--skip-torch-cuda-test
 export GRADIO_SERVER_NAME=/var/snap/stable-diffusion/common/web.socket
 export PATH=$PATH:${DIR}/python/bin
 /bin/rm -f $GRADIO_SERVER_NAME
+cd $DIR/python/webui
 exec ${DIR}/python/bin/python -u ${DIR}/python/webui/launch.py \
+  --precision full --no-half \
   --skip-prepare-environment \
   --data-dir '/var/snap/stable-diffusion/current' \
   --ckpt "$DIR/python/models/Stable-diffusion/v1-5-pruned-emaonly.safetensors"
